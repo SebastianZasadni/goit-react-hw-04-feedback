209af6248e42bc3dd5f30e527efdb57124156dc1
SebastianZasadni <112890476+SebastianZasadni@users.noreply.github.com>
2023-05-31 09:25:57 +0200

    Delete src directory

--- src/components/App.jsx
+++
@@ -1,8 +0,0 @@
-import { Section } from './components/Feedback/Section/Section.jsx';
-import { Phonebook } from './components/Phonebook/Phonebook.jsx';
-export const App = () => (
-  <>
-    <Section title="Please leave feedback" />
-    <Phonebook />
-  </>
-);
--- src/components/components/Phonebook/Filter/Filter.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const Filter = ({ onFilter }) => {
-  return <input type="text" name="filter" onChange={onFilter}></input>;
-};
-
-Filter.propTypes = {
-  onFilter: PropTypes.func,
-}
