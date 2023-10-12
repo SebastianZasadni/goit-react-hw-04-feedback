3ba32590fb3f214bceb7928324194f0a019098d8
SebastianZasadni <emblelele@gmail.com>
2023-10-12 18:25:39 +0200

    Update Statistics.jsx

---
+++ src/components/Feedback/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({
+  good,
+  bad,
+  neutral,
+  total,
+  feedbackPercentage,
+}) => {
+  return (
+    <div>
+      <span> Good: {good}</span>
+      <span> Neutral: {neutral}</span>
+      <span> Bad: {bad}</span>
+      <span> Total: {total}</span>
+      <span> Positive feedback: {feedbackPercentage}%</span>
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  bad: PropTypes.number,
+  neutral: PropTypes.number,
+};
