c64911092cdee4a107646bb90fb3c93a66a366d4
SebastianZasadni <emblelele@gmail.com>
2023-10-12 18:26:02 +0200

    Update Statistics.jsx

--- src/components/Feedback/Statistics/Statistics.jsx
+++ src/components/Feedback/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import PropTypes from 'prop-types';
 
 export const Statistics = ({
@@ -23,3 +24,30 @@ Statistics.propTypes = {
   bad: PropTypes.number,
   neutral: PropTypes.number,
 };
+=======
+import PropTypes from 'prop-types';
+
+export const Statistics = ({
+  good,
+  bad,
+  neutral,
+  total,
+  feedbackPercentage,
+}) => {
+  return (
+    <div>
+      <span> Good: {good}</span>
+      <span> Neutral: {neutral}</span>
+      <span> Bad: {bad}</span>
+      <span> Total: {total}</span>
+      <span> Positive feedback: {feedbackPercentage}%</span>
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  bad: PropTypes.number,
+  neutral: PropTypes.number,
+};
+>>>>>>> parent of dab41c7 (fix)
